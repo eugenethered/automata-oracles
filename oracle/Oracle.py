0daeda4db4d4369a89eddd83ed5e0e04a2f25c7e
Eugene the Red <me@eugenethered.com>
2022-04-29 15:25:05 +0100

    employing exchange rates

--- oracle/Oracle.py
+++ oracle/Oracle.py
@@ -1,9 +1,10 @@
 from core.oracle.Prediction import Prediction
+from exchange.rate.ExchangeRateHolder import ExchangeRateHolder
 
 
 class Oracle:
 
-    def set_data(self, data):
+    def set_exchange_rates(self, exchange_rates: ExchangeRateHolder):
         pass
 
     def predict(self, instrument) -> Prediction:
--- oracle/resolve/PredictionResolver.py
+++ oracle/resolve/PredictionResolver.py
@@ -1,6 +1,7 @@
 from typing import List
 
 from core.oracle.Prediction import Prediction
+from exchange.rate.ExchangeRateHolder import ExchangeRateHolder
 
 from oracle.Oracle import Oracle
 
@@ -10,15 +11,15 @@ class PredictionResolver:
     def __init__(self, oracles):
         self.oracles: List[Oracle] = oracles
 
-    def resolve(self, instrument, data) -> Prediction:
-        self.set_all_oracle_with_data(data)
+    def resolve(self, instrument, exchange_rates) -> Prediction:
+        self.set_all_oracle_with_exchange_rates(exchange_rates)
         predictions = self.collect_predictions_from_all_oracles(instrument)
         best_prediction = self.determine_best_profitable_prediction(predictions)
         return best_prediction
 
-    def set_all_oracle_with_data(self, data):
+    def set_all_oracle_with_exchange_rates(self, exchange_rates: ExchangeRateHolder):
         for oracle in self.oracles:
-            oracle.set_data(data)
+            oracle.set_exchange_rates(exchange_rates)
 
     def collect_predictions_from_all_oracles(self, instrument) -> List[Prediction]:
         predictions = []
