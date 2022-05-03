c3bf9852d62230113ab45c81f2c1bce443210ea9
Eugene the Red <me@eugenethered.com>
2022-05-03 12:57:53 +0100

    added exchanged from + setting required parameters

--- oracle/resolve/PredictionResolver.py
+++ oracle/resolve/PredictionResolver.py
@@ -11,9 +11,9 @@ class PredictionResolver:
     def __init__(self, oracles):
         self.oracles: List[Oracle] = oracles
 
-    def resolve(self, instrument, exchange_rates, instant) -> Optional[Prediction]:
+    def resolve(self, instrument, exchange_rates, exchanged_from=None, instant=None) -> Optional[Prediction]:
         self.set_all_oracle_with_exchange_rates(exchange_rates)
-        predictions = self.collect_predictions_from_all_oracles(instrument, instant)
+        predictions = self.collect_predictions_from_all_oracles(instrument, exchanged_from, instant)
         best_prediction = self.determine_best_profitable_prediction(predictions)
         return best_prediction
 
@@ -21,10 +21,10 @@ class PredictionResolver:
         for oracle in self.oracles:
             oracle.set_exchange_rates(exchange_rates)
 
-    def collect_predictions_from_all_oracles(self, instrument, instant) -> List[Prediction]:
+    def collect_predictions_from_all_oracles(self, instrument, exchanged_from, instant) -> List[Prediction]:
         predictions = []
         for oracle in self.oracles:
-            prediction = oracle.predict(instrument, instant)
+            prediction = oracle.predict(instrument, exchanged_from, instant)
             predictions.append(prediction)
         valid_predictions = [p for p in predictions if p is not None]
         return valid_predictions
