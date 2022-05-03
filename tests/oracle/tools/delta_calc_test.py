1a53908841240a6da8f551cb37d287a4bdfd3050
Eugene the Red <me@eugenethered.com>
2022-05-03 17:19:32 +0100

    added delta tools

---
+++ tests/oracle/tools/delta_calc_test.py
@@ -0,0 +1,42 @@
+import unittest
+
+from core.exchange.InstrumentExchange import InstrumentExchange
+from core.number.BigFloat import BigFloat
+from exchange.rate.InstantRate import InstantRate
+
+from oracle.tools.delta_calc import calc_delta, calc_delta_prediction
+
+
+class DeltaCalcTestCase(unittest.TestCase):
+
+    def test_should_compute_instant_rate_delta(self):
+        rate = InstantRate(2, BigFloat('150.00'))
+        other_rate = InstantRate(1, BigFloat('100.00'))
+        delta = calc_delta(rate, other_rate)
+        self.assertEqual(delta, BigFloat('50.00'))
+
+    def test_should_compute_negative_instant_rate_delta(self):
+        rate = InstantRate(2, BigFloat('100.00'))
+        other_rate = InstantRate(1, BigFloat('150.00'))
+        delta = calc_delta(rate, other_rate)
+        self.assertEqual(delta, BigFloat('-50.00'))
+
+    def test_should_compute_delta_prediction(self):
+        rate = InstantRate(2, BigFloat('150.00'))
+        other_rate = InstantRate(1, BigFloat('100.00'))
+        instrument_exchange = InstrumentExchange('OTC', 'GBP')
+        delta_prediction = calc_delta_prediction(rate, other_rate, instrument_exchange)
+        self.assertEqual(delta_prediction.outcome, ['OTC', 'GBP'])
+        self.assertEqual(delta_prediction.profit, BigFloat('50.00'))
+
+    def test_should_compute_negative_delta_prediction(self):
+        rate = InstantRate(2, BigFloat('100.00'))
+        other_rate = InstantRate(1, BigFloat('150.00'))
+        instrument_exchange = InstrumentExchange('OTC', 'GBP')
+        delta_prediction = calc_delta_prediction(rate, other_rate, instrument_exchange)
+        self.assertEqual(delta_prediction.outcome, ['OTC', 'GBP'])
+        self.assertEqual(delta_prediction.profit, BigFloat('-50.00'))
+
+
+if __name__ == '__main__':
+    unittest.main()
